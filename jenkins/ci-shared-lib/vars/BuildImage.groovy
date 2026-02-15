23c4c6b96f3e10bcfe2ccce3f8f1a7c20fbd9517
Mohamed <therealqi@hotmail.com>
2026-02-16 00:42:36 +0200

    Fixed shared library quotation issue

--- jenkins/ci-shared-lib/vars/BuildImage.groovy
+++ jenkins/ci-shared-lib/vars/BuildImage.groovy
@@ -2,7 +2,7 @@ def call(String workdir, String imageRepo, String imageName, String imageTag) {
     dir(workdir) {
         sh """
             echo "Building Docker image ${imageRepo}/${imageName}:${imageTag} from directory ${workdir}"
-            docker build -t ${imageRepo}/${imageName}:${imageTag} ."
+            docker build -t ${imageRepo}/${imageName}:${imageTag} .
             echo "Successfully built Docker image ${imageRepo}/${imageName}:${imageTag}"
         """
     }
