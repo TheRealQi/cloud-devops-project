d5356a4290e90be2e591bda02abe376b40faec58
Mohamed <therealqi@hotmail.com>
2026-02-17 21:28:32 +0200

    Changed Git repo logic

--- jenkins/ci-shared-lib/vars/PushK8sManifests.groovy
+++ jenkins/ci-shared-lib/vars/PushK8sManifests.groovy
@@ -1,13 +1,13 @@
-def call(String workdir, String imageTag) {
+def call(String workdir, String imageTag, String gitRepoUrl, String branchName) {
     dir(workdir) {
+        def cleanUrl = gitRepoUrl.replace("https://", "")
         sh """
-            echo "Pushing updated manifests to GitHub"
+            echo "Pushing updated manifests to ${cleanUrl} on branch ${branchName}"
             git config user.email "jenkins@clouddevops.com"
             git config user.name "jenkins"
             git add .
             git commit -m "Update image to ${imageTag}" || true
-            git push https://${GIT_USER}:${GIT_PASS}@github.com/therealqi/clouddevopsgradproject.git HEAD:main
-            echo "Manifests pushed successfully"
+            git push https://${GIT_USER}:${GIT_PASS}@${cleanUrl} HEAD:${branchName}
         """
     }
 }
