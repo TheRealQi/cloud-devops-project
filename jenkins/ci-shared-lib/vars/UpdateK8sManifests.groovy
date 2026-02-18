212edd53b21eca16a63c6660313b4e86838b0a6b
Mohamed <therealqi@hotmail.com>
2026-02-18 17:08:44 +0200

    Edited ArgoCD App. file and jenkinsfile.

--- jenkins/ci-shared-lib/vars/UpdateK8sManifests.groovy
+++ jenkins/ci-shared-lib/vars/UpdateK8sManifests.groovy
@@ -1,8 +1,8 @@
-def call(String workdir,String imageName, String imageTag) {
+def call(String workdir,String ecrURI, String imageTag) {
     dir(workdir) {
         sh """
         echo 'Editing App. Deployment Manifest'
-        sed -i 's|image:.*|image: ${imageName}:${imageTag}|g' deployment.yaml
+        sed -i 's|image:.*|image: ${ecrURI}:${imageTag}|g' deployment.yaml
         echo 'App. Deployment Manifest edited successfully'
         """
     }
